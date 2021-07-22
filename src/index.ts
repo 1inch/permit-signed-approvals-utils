a2e32e497484f0439d179841bf393aa0ba6140d6
shoom3301 <shoom3301@gmail.com>
2021-07-22 13:14:06 +0400

    feat: index.ts file for short imports

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export * from './connector/private-key-provider.connector';
+export * from './connector/provider.connector';
+export * from './connector/web3-provider.connector';
+
+export * from './model/abi.model';
+export * from './model/chain.model';
+export * from './model/eip712.model';
+export * from './model/permit.model';
+export * from './eip-2612-permit.const';
+export * from './eip-2612-permit.helper';
+export * from './eip-2612-permit.utils';
